7c24d40eddbc7a24a37ee14b552afecf8f2b3803
Тонконожка <tonkonogdom@MacBookPro.nnov.corp.local>
2025-11-01 14:21:54 +0300

    new update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import './index.css'
-import jpg from './images/image.jpg'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const image = new Image()
-  image.src = jpg
-  document.querySelector('.images').appendChild(image)
-})
